dc7c25eb768bb4184c764be60d5ecf5d7eee9698
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-12 14:35:13 +0200

    Init Vite project

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+  export default{
+    data(){
+      return{
+
+      }
+    }
+  }
+</script>
+
+<template>
+ <h1>Hello World</h1>
+</template>
+
+<style>
+body{
+  background-color: rgb(70, 70, 70);
+}
+</style>
